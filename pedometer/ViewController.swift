05b9392da9fd7c1351e8401a4c1f54006da306d0
gentom <gento.m.developer@gmail.com>
2017-09-06 21:49:52 +0900

    commit

--- pedometer/ViewController.swift
+++ pedometer/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counter: UILabel!
+    // init
+    let pedometer = CMPedometer()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if(CMPedometer.isStepCountingAvailable()){
+            
+            self.pedometer.startUpdates(from: NSDate() as Date) {
+                (data: CMPedometerData?, error) -> Void in
+                DispatchQueue.main.async(execute: { () -> Void in
+                    if(error == nil){
+                        // step num
+                        let steps = data!.numberOfSteps
+                        self.counter.text = "steps: \(steps)"
+                    }
+                })
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
